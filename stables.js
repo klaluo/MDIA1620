b34395335fd5491049c52315d16589135691de88
Kayla Luo <kluo17@my.bcit.ca>
2024-10-11 15:38:24 -0700

    Lab 6 Codes

--- stables.js
+++ stables.js
@@ -1,7 +1,7 @@
 let horseName = "Snowdog";  
-console.log(horseName);
+console.log[horseName];
 let horseAge = 5;
-console.log(horseAge);
+console.log[horseAge];
 let isHorseInside = true;
 console.log(isHorseInside);
 
@@ -23,7 +23,7 @@ console.log(false + "NaN");
 let horseNickname = "Icey";
 
 console.log(`My beautiful horse's name is ${horseName} and their nickname is ${horseNickname}. `);
-console.log("3 months stay at my stable is $" + (STABLES_MONTHLY_FEE * 3) + "With a 10% discount, you would save $" + (STABLES_MONTHLY_FEE * 3)*0.1);
+console.log("3 months stay at my stable is $" + (STABLES_MONTHLY_FEE * 3) + " With a 10% discount, you would save $" + (STABLES_MONTHLY_FEE * 3)*0.1);
 
 
 // lesson-5 code 
@@ -41,6 +41,33 @@ if (!isHorseInside){
 }
 
 
+// lesson 6 code
+//horse = 
+let word = "rainbow";
+console.log(word.length);
 
+let horseNames = [horseName, "Strawberry", "Tank"];
+let horseAges = [horseAge, 5, 3];
+let horseLocations = [isHorseInside, "inside", "outside"];
+// console.log(horseNames[2]);
+// console.log("welcome to my stables! There are " + horseNames.length + "horses staying here!")
+// console.log(`Welcome to my stables! There are ${horseNames.length} horses staying here! There names are ${horseNames}`)
+//  console.log(horseAges[2]);
+// console.log(horseLocation[2]);
+
+// if(horseNames[0] === "Snowdog") {
+//     console.log("Hi Snowdog")
+// }
+
+// console.log(horseAges[2] + horseAges[1]); //8
+
+// if(horseNames.length < 5) {
+//     console.log("These stables are unpopular");
+// }
+
+// Lab 6
+
+console.log(`Welcome to my stables! I have ${horseNames.length} horses that live here. Their names are ${horseNames} and they are all very friendly. 
+    They are ${horseLocations[1]}. There ages are ${horseAges}`)
 
 
