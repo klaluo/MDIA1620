1406aace79da2d46d2bbf480d343922487388908
Kayla Luo <kluo17@my.bcit.ca>
2024-12-06 13:29:17 -0800

    completed final stables kayla

--- stables.js
+++ stables.js
@@ -1,135 +1,223 @@
-let horse = {}
-console.log(horse);
-let horseArray = []
 
-let horseArray = ["strawberry","shortcake", 3, true];
-let car = {
-    color: "Red"
-}
+//------------------------- Setting up shop -------------------------//
 
 
-function intro(horse)
+let horses = [];
+let visitorMessage = "Welcome to my stables, visitor!";
+let latePaymentFee = 200;
+let availableStalls = 8
+console.log(visitorMessage);
+
+//------------------------- First day -------------------------//
 
 
-
-
-
-
-
-
-console.log("My horse name is called" + horse.name + "and they are")
-
-function createHorse(name, nickname, age, faveTreat, isInside) {
-    const tempObj = {};
-
-    tempObj.name = name;
-    tempObj.nickmame = nickname;
-    tempObj.age = age;
-    tempObj.faveTreat = faveTreat
-    tempObj.isInside = isInsdie; 
-    tempObj. rent + 123;
-    tempObj.intro = function () {
-        console.log(
-            `My horse is called ${this.name} and they are ${this.age} years old!`
-        )
+function Horse(name, nickname, favTreat, age, isInside, isSmelly, hobby){
+    this.name = name;
+    this.nickname = nickname;
+    this.favTreat = favTreat;
+    this.age = age;
+    this.monthlyRent = 500;
+    this.isInside = isInside;
+    this.isSmelly = isSmelly;
+    this.hobby = hobby;
+    this.introduction = function(){
+        return `This is ${this.name}. She loves to ${this.hobby} and is ${this.age} years old. She loves to eat ${this.favTreat}`
     }
-    return tempObj;
+    this.funFact = function(){
+        return `Fun Fact! This horses' nickname is ${this.nickname}!`
+    }
 }
 
-const strawberry = createHorse("Strawberry", "Shortcake", 3, "carrots", true);
-console.log(strawberry);
+const snowy = new Horse ("Snowy", "SnowSnow", "blueberries", 13, true, false, "playing fetch");
+const muffy = new Horse ("Muffy", "Murf", "toilet paper", 20, true, true, "smelling people's feet" );
+const tank = new Horse  ("Tank", "Smelly", "tree branches", 3, false, true, "spinning in circles");
+horses.push(snowy, muffy, tank);
+
+let ellie = {
+    name: "Ellie",
+    nickname: "Barker",
+    favTreat: "strawberries",
+    age: 5,
+    monthlyRent: 500,
+    isInside: false,
+    isSmelly: true,
+    hobby: "chasing rats",
+}
+horses.push(ellie);
+
+snowy.isHungry = true;
+muffy.isHungry = false;
+tank.isHungry = false;
+ellie.isHungry = true;
+
+console.log(horses)
 
 
 
+// {
+    //  {
+    //     name: 'Snowy',
+    //     nickname: 'SnowSnow',
+    //     favTreat: 'blueberries',
+    //     age: 13,
+    //     monthlyRent: 500,
+    //     isInside: true,
+    //     isSmelly: false,
+    //     hobby: 'playing fetch',
+    //     introduction: [Function (anonymous)],
+    //     funFact: [Function (anonymous)]
+    //   },
+    //    {
+    //     name: 'Muffy',
+    //     nickname: 'Murf',
+    //     favTreat: 'toilet paper',
+    //     age: 20,
+    //     monthlyRent: 500,
+    //     isInside: true,
+    //     isSmelly: true,
+    //     hobby: "smelling people's feet",
+    //     introduction: [Function (anonymous)],
+    //     funFact: [Function (anonymous)]
+    //   },
+    // {
+    //     name: 'Tank',
+    //     nickname: 'Smelly',
+    //     favTreat: 'tree branches',
+    //     age: 3,
+    //     monthlyRent: 500,
+    //     isInside: false,
+    //     isSmelly: true,
+    //     hobby: 'spinning in circles',
+    //     introduction: [Function (anonymous)],
+    //     funFact: [Function (anonymous)]
+    //   },
+    // {
+    //     name: 'Ellie',
+    //     nickname: 'Barker',
+    //     favTreat: 'strawberries',
+    //     age: 5,
+    //     monthlyRent: 500,
+    //     isInside: false,
+    //     isSmelly: true,
+    //     hobby: 'chasing rats',
+    //     introduction: [Function (anonymous)],
+    //     funFact: [Function (anonymous)]
+    //   }
 
-/*let horseName = "Snowdog";  
-console.log[horseName];
-let horseAge = 5;
-console.log[horseAge];
-let isHorseInside = true;
-console.log(isHorseInside);
-
-// LESSON-4 code
-// console.log(typeof horseName)
-// console.log(typeof horseAge)
-// const STABLES_MONTHLY_FEE = 1000;
-// console.log(STABLES_MONTHLY_FEE);
-
-// console.log(STABLES_MONTHLY_FEE - 20);
-// console.log("my stable monthly fee is " + (STABLES_MONTHLY_FEE - 20));
-// console.log(`my horse's name is ${horseName} and he is ${horseAge}`)
-// console.log("hi" + " 2"); // type coercion 
-// console.log(2 - "2"); // type coercion
-// console.log("hi " + "there");
-// console.log(false + "NaN");
-
-// assignment 
-let horseNickname = "Icey";
-
-// console.log(`My beautiful horse's name is ${horseName} and their nickname is ${horseNickname}. `);
-// console.log("3 months stay at my stable is $" + (STABLES_MONTHLY_FEE * 3) + " With a 10% discount, you would save $" + (STABLES_MONTHLY_FEE * 3)*0.1);
+//---------------------- Growing business ----------------------//
 
 
-// lesson-5 code 
 
-// if (horseName !== "Muffy") {
-//     console.log(`${horseName} has come to visit the stables`)
-// } else {
-//     console.log (horseName + "is lonely");
-// }
+availableStalls -= horses.length
 
-// if (!isHorseInside){
-//     console.log(`${horseName}is outside`);
-// } else {
-//     console.log(`${horseName} is inside`)
-// }
+    if (availableStalls < 2) {
+        console.log("We need to build more stalls");
+    } else {
+        console.log(`We have ${availableStalls} available!`);
+    }
 
 
-// lesson 6 code
-//horse = 
-// let word = "rainbow";
-// console.log(word.length);
-
-let horseNames = [horseName, "Strawberry", "Tank"];
-let horseAges = [horseAge, 5, 3];
-let horseLocations = [isHorseInside, true, false];
-// console.log(horseNames[2]);
-// console.log("welcome to my stables! There are " + horseNames.length + "horses staying here!")
-// console.log(`Welcome to my stables! There are ${horseNames.length} horses staying here! There names are ${horseNames}`)
-//  console.log(horseAges[2]);
-// console.log(horseLocation[2]);
-
-// if(horseNames[0] === "Snowdog") {
-//     console.log("Hi Snowdog")
-// }
-
-// console.log(horseAges[2] + horseAges[1]); //8
-
-// if(horseNames.length < 5) {
-//     console.log("These stables are unpopular");
-// }
-
-// Lab 6
-
-console.log(`Welcome to my stables! I have ${horseNames.length} horses that live here. Their names are ${horseNames} and they are all very friendly. Their ages are ${horseAges}`)
-if(horseLocations[0]) {
-    console.log(horseNames[0] + " is inside")
-} else {
-    console.log(horseNames[0] + " is outside")
+function calculateLatePayment(monthlyRent, latePaymentFee) {
+    let totalPayment = monthlyRent + latePaymentFee;
+    console.log(`The total payment including late fee is: $${totalPayment}`);
 }
 
-if(horseLocations[1]) {
-    console.log(horseNames[1] + " is inside")
-} else {
-    console.log(horseNames[1] + " is outside")
+calculateLatePayment(snowy.monthlyRent, latePaymentFee);
+
+
+findWhoLikesTreats(toilet paper);
+for (let i = 0; i < horses.length; i++) {
+    let horse = horses[i];
+    let horseName = horse["name"];
+    let favorite = horse["favTreat"];
+    let doesLikeToiletPaper = favorite === "toilet paper";
+
+    if (doesLikeToiletPaper) {
+        console.log(`${horseName} loves toilet paper!`);
+    } else{
+        console.log(
+            `${horseName} doesn't like toilet paper, only ${favorite}, keep checking!`
+        );
+    }
 }
 
-if(horseLocations[2]) {
-    console.log(horseNames[2] + " is inside")
-} else {
-    console.log(horseNames[2] + " is outside")
-}
-*/
 
+function getHorseNickname (horseName) {
+    for (let i = 0; i < horse.length; i++) {
+        if (horses[i].name === horseName){
+            return horses[i].nickname;
+        }
+    }
+    return horseName.nickname
+}
+
+getHorseNickname("snowy");
+
+
+
+
+//------------------------- Day to day operations -------------------------//
+
+
+function getHobby(horseName) {
+    for (let i = 0; i < horses.length; i++) {
+        let horse = horses[i];
+        if (horse.name === horseName) {
+            console.log(`My horse ${horse.name}'s hobby is to ${horse.hobby}`);
+            return;
+        }
+    }
+}
+getHobby(snowy);
+
+
+function moveHorsesOutside() {
+    for (let i = 0; i < horses.length; i++) {
+        horses[i].isInside = false;
+        console.log(`${horses[i].name} has been moved outside to spend time in the sun.`);
+    }
+}
+
+
+moveHorsesOutside();
+
+
+
+
+Horse.prototype.moveLocation = function() {
+    if (this.isInside) {
+        this.isInside = false;
+        console.log(`${this.name} has been moved outside.`);
+    } else {
+        this.isInside = true;
+        console.log(`${this.name} has been moved inside`);
+    }
+};
+
+
+function feedHorses(horses) {
+    console.log(`It's time to eat!`);
+    horses.forEach(function(horse) {
+        if (horse.isInside === false) {
+            horse.isInside = true;
+            console.log(`${horse.name} was outside and now inside.`);
+        } else {
+            console.log(`${horse.name} is already inside and is ready for a treat!`);
+        }
+        console.log(`${horse.name} gets their favorite treat: ${horse.favTreat}.`);
+    });
+}
+
+feedHorses(horses);
+
+
+horses.forEach(function(horse) {
+    horse.moveLocation();
+});
+
+console.log("GOODNIGHT!");
+horses.forEach(function(horses) {
+    timeOfDay(horses, true); 
+});
 
 
